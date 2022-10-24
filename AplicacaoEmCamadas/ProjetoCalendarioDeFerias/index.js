2b055c949b47f7ada0cf3be12914fb3ecd436e72
Otávio Henrique Moreira Campagnoli <otavio.Campagnoli@rift.local>
2022-10-24 15:57:51 -0300

    Aplicação em camadas: Projeto feriados mais próximos.

---
+++ AplicacaoEmCamadas/ProjetoCalendarioDeFerias/index.js
@@ -0,0 +1,11 @@
+import retornaProximosFeriados from './retornaFeriados.js';
+import imprimeFeriados from './imprimeFeriados.js'
+
+console.log('\nOs próximos feriados serão:\n')
+
+let numeroFeriados = 5;
+
+let proximosFeriados = retornaProximosFeriados(numeroFeriados);
+imprimeFeriados(proximosFeriados);
+
+ 
